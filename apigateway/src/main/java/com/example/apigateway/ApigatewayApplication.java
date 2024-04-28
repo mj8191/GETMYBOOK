533bf0ae4fbfb18cede10eb1eb255760818d100a
mkmishra8191 <manjeet.eng8191@gmail.com>
2024-04-28 19:00:27 +0530

    Initial Commit

--- apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -1,7 +1,12 @@
 package com.example.apigateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
 
 @SpringBootApplication
 public class ApigatewayApplication {
@@ -9,5 +14,8 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
+
+
+
 
 }
