93b3edfab70e8753df6f6e1fa105e944a3962322
mkmishra8191 <manjeet.eng8191@gmail.com>
2024-09-01 00:27:33 +0530

    test

--- book/src/main/java/com/getmybook/book/Controller.java
+++ book/src/main/java/com/getmybook/book/Controller.java
@@ -86,7 +86,7 @@ public class Controller {
 
     @GetMapping("/getAllBook")
     public List getAllBook(){
-        List<Book> list = bookRepository.getAllByStatus("Published");
+        List<Book> list = bookRepository.findAll();
         if(list.isEmpty()) {
             return Collections.EMPTY_LIST;
         }
@@ -96,8 +96,8 @@ public class Controller {
     }
 
     @GetMapping("getAllByStatus")
-    public List getAllByStatus(@RequestParam String status){
-        List<Book> list = bookRepository.getAllByStatus(status);
+    public List getAllByStatus(){
+        List<Book> list = bookRepository.getAllByStatus("Published");
         if(list.isEmpty()) {
             return Collections.EMPTY_LIST;
         }
